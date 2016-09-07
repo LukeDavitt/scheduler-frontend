6d3aee5cc54a40f459ae0ac90f614687f1633b2e
Luke Davitt <luke.davitt1@gmail.com>
2016-09-07 09:40:26 -0400

    basic styling structure

--- app/index.js
+++ app/index.js
@@ -1,13 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Router, Route, IndexRoute, hashHistory } from "react-router"
+import "./styles/reset.css"
+import { default as Home } from "./components/Home"
+import { default as Settings } from "./components/Settings"
 
-class HelloWorld extends React.Component {
-  render() {
-    return (<div>Hello World!</div>)
-  }
-}
+const App = props => (<div>{props.children}</div>)
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="settings" component={Settings} />
+    </Route>
+  </Router>,
   document.getElementById("root")
 )
