75ee7e4cd3ef2c45d4f55ca5886f4a27de6c081d
Luke Davitt <luke.davitt1@gmail.com>
2016-09-07 12:33:45 -0400

    baseline login page

---
+++ app/components/Button/README.md
@@ -0,0 +1,6 @@
+Button.propTypes = {
+  style: React.PropTypes.object,
+  onClick: React.PropTypes.func,
+  type: React.PropTypes.string.isRequired,
+  children: React.PropTypes.node.isRequired
+}
--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -1,11 +1,27 @@
 import React from "react"
+import cssModules from "react-css-modules"
+import style from "./style.css"
+
+import { default as Signup } from "../Signup"
+import { default as Login } from "../Login"
 
 export class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      formState: "signup"
+    }
+  }
+
   render() {
     return (
-      <div>Home component</div>
+      <div className={style.leader}>
+        <h1 className={style.title}>Moser Time Tracker</h1>
+        {this.state.formState === "signup" ? <Signup /> : null}
+        {this.state.formState === "login" ? <Login /> : null}
+      </div>
     )
   }
 }
 
-export default Home
+export default cssModules(Home, style)
